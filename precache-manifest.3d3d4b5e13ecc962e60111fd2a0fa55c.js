51fd3441091470ce34bda917c10db2e1195f796d
Benjamin Leeds <baleeds@ncsu.edu>
2019-08-29 11:48:11 -0400

    Updates

--- precache-manifest.3d3d4b5e13ecc962e60111fd2a0fa55c.js
+++ precache-manifest.3d3d4b5e13ecc962e60111fd2a0fa55c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/bn-react/static/js/runtime~main.822ea88e.js"
   },
   {
-    "revision": "bfb9bf956a9889bec9e9",
-    "url": "/bn-react/static/js/main.bfb9bf95.chunk.js"
+    "revision": "6c394da3208df9276168",
+    "url": "/bn-react/static/js/main.6c394da3.chunk.js"
   },
   {
     "revision": "e3083aef0e0b850df9a9",
     "url": "/bn-react/static/js/2.e3083aef.chunk.js"
   },
   {
-    "revision": "bfb9bf956a9889bec9e9",
-    "url": "/bn-react/static/css/main.f4177286.chunk.css"
+    "revision": "6c394da3208df9276168",
+    "url": "/bn-react/static/css/main.8b7854bb.chunk.css"
   },
   {
-    "revision": "565865a1b37d426d1ccdfc3d794d3183",
+    "revision": "239756b2f02453c5d9afe9b071046602",
     "url": "/bn-react/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bn-react/precache-manifest.309cf69e966505743569edf5e5c158b5.js"
+  "/bn-react/precache-manifest.3d3d4b5e13ecc962e60111fd2a0fa55c.js"
 );
 
 workbox.clientsClaim();
