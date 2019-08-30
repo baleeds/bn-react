cc47edd7575f5cd839d2e630e6a7b9a659435358
Benjamin Leeds <baleeds@ncsu.edu>
2019-08-30 16:46:24 -0400

    Updates

---
+++ precache-manifest.0d2df76cee91e98c05a24756cf89039e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "822ea88eaa29c2e6698b",
+    "url": "/bn-react/static/js/runtime~main.822ea88e.js"
+  },
+  {
+    "revision": "33d629e15f5ab1f73f6f",
+    "url": "/bn-react/static/js/main.33d629e1.chunk.js"
+  },
+  {
+    "revision": "8e237a303562dab8e9fb",
+    "url": "/bn-react/static/js/2.8e237a30.chunk.js"
+  },
+  {
+    "revision": "33d629e15f5ab1f73f6f",
+    "url": "/bn-react/static/css/main.5ad256ca.chunk.css"
+  },
+  {
+    "revision": "c6c94fb6a7d27ccbabf87ecbd5930d8d",
+    "url": "/bn-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35f2def5311b72a6e2adeec2d545f707.js"
+  "/bn-react/precache-manifest.0d2df76cee91e98c05a24756cf89039e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/bn-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
